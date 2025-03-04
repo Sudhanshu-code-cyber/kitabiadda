de7f7ab442b5bfafe791eba11ac26e5588829a4f
Sudhanshu-code-cyber <sudhanshuaryan2005@gmail.com>
2025-03-04 21:56:37 +0530

    index

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head>
-<body>
-    <h1>Hello World</h1>
-    <?php include_once "subheader.php";?>
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" /></head>
+<body class="bg-[#FBFFE4]">
+    <?php include_once "includes/header.php";?>
+    <?php include_once "includes/subheader.php";?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
 </html>
