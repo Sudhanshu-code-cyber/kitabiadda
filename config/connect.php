eccebc161143a139d30d00695fe9ba65cce0e376
theaman82 <theaman6826@gmail.com>
2025-03-05 20:10:46 +0530

    global

--- config/connect.php
+++ config/connect.php
@@ -1,6 +1,17 @@
 <?php
     $connect = new mysqli("localhost","root", "","readrainbow") or die("error connecting to database");
 
+    session_start();
+
+    // get user information
+    function getUser(){
+            global $connect;
+            $email = $_SESSION['user'];
+            $query = $connect->query("select * from users where email='$email'");
+            $userData = $query->fetch_array();
+            return $userData;
+    }
+
 // message 
 function message($mass)
 {
