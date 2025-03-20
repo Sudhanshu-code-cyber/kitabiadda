9a28638ba4852f11a187d8704c029d927e0025e5
Sudhanshu-code-cyber <sudhanshuaryan2005@gmail.com>
2025-03-20 19:30:47 +0530

    oldbook completed

--- includes/oldBook.php
+++ includes/oldBook.php
@@ -54,7 +54,7 @@ $booksQuery = $connect->query("SELECT * FROM books WHERE version='old'");
                         <!-- Discount Badge (60% Off) -->
                         <div
                             class="absolute left-2 top-2 bg-red-500 text-white px-3 py-1 text-xs font-bold rounded-md shadow-md">
-                            60% OFF
+                            <?=round($percentage);?>% OFF
                         </div>
 
                         <!-- Wishlist Heart Icon (Prevents Click from Going to Next Page) -->
