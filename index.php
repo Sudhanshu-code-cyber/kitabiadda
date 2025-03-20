70451fc1a4ffad979df162a69e56def113f188e8
Sudhanshu-code-cyber <sudhanshuaryan2005@gmail.com>
2025-03-20 20:32:44 +0530

    name

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ReadRainbow | Book</title>
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
 </head>
