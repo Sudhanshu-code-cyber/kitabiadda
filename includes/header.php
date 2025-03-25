b9c213407d6f9709c150ddfb16a52fcc022b818e
theaman82 <amankumaraman55555@gmail.com>
2025-03-25 23:14:53 +0530

    header all notification fixes except chatboard

--- includes/header.php
+++ includes/header.php
@@ -42,7 +42,7 @@ $userEmail = $user['email'];
                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
         </svg>
         <span class="sr-only">Notifications</span>
-        <?php $countcartQuery = $connect->query("select * from cart where email='$userEmail' and direct_buy=1");
+        <?php $countcartQuery = $connect->query("select * from cart where email='$userEmail' and direct_buy=0");
         $countcart = $countcartQuery->num_rows;
         if ($countcart > 0):
             ?>
@@ -96,7 +96,7 @@ $userEmail = $user['email'];
                         Profile</a>
                 </li>
                 <li>
-                    <a href="#"
+                    <a href="contact.php "
                         class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Help</a>
                 </li>
                 <li>
