40438c905253901c13dc055f259cf194b239f2f8
vikashaaryan <vikasharyan323@gmail.com>
2025-04-04 13:01:07 +0530

    cout

--- includes/header.php
+++ includes/header.php
@@ -100,17 +100,29 @@ $userEmail = $user ? $user['email'] : null;
                         d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
                 </svg>
                 <?php
-                $chatCountQuery = $connect->query("SELECT COUNT(DISTINCT books.id) as total_chats 
-                 FROM message 
-                 JOIN books ON message.product_id = books.id 
-                WHERE message.sender_id = '$userId' OR message.receiver_id = '$userId'");
-                $chatCountData = mysqli_fetch_assoc($chatCountQuery);
-                $totalChats = $chatCountData['total_chats'] ?? 0;
+                if (isset($userId)) {
+                    $chatCountQuery = $connect->query("
+                   SELECT COUNT(DISTINCT books.id) AS total_chats
+                  FROM message
+                   JOIN books ON message.product_id = books.id
+                    WHERE message.sender_id = '$userId' OR message.receiver_id = '$userId'
+                   ");
+
+                    if ($chatCountQuery && $chatCountQuery->num_rows > 0) {
+                        $chatCountRow = $chatCountQuery->fetch_assoc();
+                        $chatCountData = $chatCountRow['total_chats'];
+
+                          if ($chatCountData > 0) {
+                  ?>
+                            <div class="absolute -top-1 -right-1 inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 border border-white rounded-full transform group-hover:scale-110 transition-transform">
+                                <?= $chatCountData; ?>
+                            </div>
+                  <?php
+                        }
+                    }
+                  }
                 ?>
-                <div
-                    class="absolute -top-1 -right-1 inline-flex items-center justify-center w-5 h-5 text-xs font-bold text-white bg-red-500 border border-white rounded-full transform group-hover:scale-110 transition-transform">
-                    <?= $totalChats; ?>
-                </div>
+
             </a>
         </div>
 
